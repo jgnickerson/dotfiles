2ca0bf73010d0b85858e0d2a50ee5901b9c391f4
jgnickerson <jgnickerson@me.com>
2021-07-06 08:19:22 +0100

    Remove unnecessary aliases

--- alias.zsh
+++ alias.zsh
@@ -1,10 +1,7 @@
 # cd ~/.oh-my-zsh/custom
 # ln -sv /path/to/this/file
 
-alias prb="ssh -c 3des-cbc -i ~/.ssh/tf_eu ec2-user@34.250.215.117"
-
-alias bast="gcloud compute --project \"international-gramercy\" ssh --zone \"europe-west1-b\" \"international-gramercy-bastion\""
-
-alias dbgw="ssh -i ~/.ssh/tf_eu ec2-user@34.250.75.8"
-
 alias gs='git status'
+
+alias di='docker images'
+alias dih='docker images | head'
