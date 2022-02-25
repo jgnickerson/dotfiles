a1d58249a1c52615d2a501111626b919209b4d7c
jgnickerson <jgnickerson@me.com>
2022-02-25 11:18:21 +0000

    Refactor funcs

--- .dotfiles/alias.zsh
+++ .dotfiles/alias.zsh
@@ -4,14 +4,21 @@
 alias dots="git --git-dir=$HOME/.dotfiles-git --work-tree=$HOME"
 
 alias gs='git status'
-alias gdb='del-branches'
-alias dka='docker-kill-all'
-alias mpd='main-pull-del'
-alias gaf='git-add-files'
-alias funcs='ls ~/personal/dotfiles/functions'
 
 alias di='docker images'
 alias dih='docker images | head'
 alias kx='kubectx'
 alias kn='kubens'
 alias p='pulumi'
+
+# Zsh Funcs
+alias funcs='ls ~/.dotfiles/functions'
+alias gdb='del-branches'
+alias dka='docker-kill-all'
+alias mpd='main-pull-del'
+alias gaf='git-add-files'
+alias grf='git-status-fzf -p git restore'
+alias cop='checkout-pop'
+alias sco='stash-checkout'
+alias ke='kube-exec'
+
